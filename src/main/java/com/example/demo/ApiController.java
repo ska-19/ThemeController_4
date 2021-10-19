975f3a101304bef3d10850afe19c6eaf06f16842
ska-19 <ska04@icloud.com>
2021-10-19 21:00:55 +0300

    на 4

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -20,7 +20,7 @@ public class ApiController {
     private List<String> messages = new ArrayList<>();
 
     // d) Выводит список сообщений Выводит сообщение по индексу
-    /*  */
+    /* curl -X GET http://localhost:8080/messages -H 'Content-Type: text/plain' */
     @GetMapping("messages")
     public List<String> getMessages() {
         return messages;
@@ -48,7 +48,7 @@ public class ApiController {
     }
 
     // e) Обновляет сообщение по индексу
-    /* curl -X PUT http://localhost:8080/messages/0 -H 'Content-Type: text/plain' -d 'ttttttt' */
+    /* curl -X PUT http://localhost:8080/messages/1 -H 'Content-Type: text/plain' -d 'ttttttt' */
     @PutMapping("messages/{index}")
     public void updateMessage(
             @PathVariable("index") Integer i,
@@ -56,4 +56,49 @@ public class ApiController {
         messages.remove((int) i);
         messages.add(i, message);
     }
+
+    // ДАЛЬШЕ ДЗ НА 4
+
+    // 1. возвращает индекс первого текста с подстрокой text
+    /* curl -X GET http://localhost:8080/messages/search/tt -H 'Content-Type: text/plain' */
+    @GetMapping("messages/search/{text}")
+    public Integer getIndexSearch(@PathVariable("text") String text) {
+        for (int i = 0; i < messages.size(); i++) {
+            String E = messages.get(i);
+            if(E.contains(text)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 2. возвращает количество сообщений
+    /* curl -X GET http://localhost:8080/messages/count -H 'Content-Type: text/plain' */
+    @GetMapping("messages/count")
+    public Integer getCount() {
+        return messages.size();
+    }
+
+    // 3. Добавляет сообщение с порядковым номером index
+    /* curl -X POST http://localhost:8080/messages/1/create -H 'Content-Type: text/plain' -d 'ffffff' */
+    @PostMapping("messages/{index}/create")
+    public void addMessageToIndex(
+            @PathVariable("index") Integer index,
+            @RequestBody String text) {
+        messages.add(index, text);
+    }
+
+    // 4. Удаляет все сообщения в которых есть подстрока text
+    /* curl -X DELETE http://localhost:8080/messages/search/aaa -H 'Content-Type: text/plain' */
+    @DeleteMapping("messages/search/{text}")
+    public void deleteTextSearchText(@PathVariable("text") String text) {
+        for (int i = 0; i < messages.size(); i++) {
+            String E = messages.get(i);
+            if(E.contains(text)){
+                messages.remove(i);
+            }
+        }
+    }
+
+
 }
